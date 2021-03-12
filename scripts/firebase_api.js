0f895ca000be84ea4f455dce70065233b5c7b292
Miguel Marasigan <miguelmarasigan55@gmail.com>
2021-03-11 20:27:18 -0800

    Configured initial Firebase setup, created barebones profile.html/css

---
+++ scripts/firebase_api.js
@@ -0,0 +1,12 @@
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBPA589ZQIHvhNczzNO8mD93R6Szih1l1M",
+        authDomain: "streamlinedapi.firebaseapp.com",
+        projectId: "streamlinedapi",
+        storageBucket: "streamlinedapi.appspot.com",
+        messagingSenderId: "1005119766948",
+        appId: "1:1005119766948:web:d117b748042cca606d0131"
+    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
