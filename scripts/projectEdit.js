316fa51da8b43fd5a8daa0a23fcfb0d3d25ab961
Raymond <70726900+ray165@users.noreply.github.com>
2021-03-27 21:26:36 -0700

    projectEdit.js successfully updates values to firestore! Also autofills current values.

--- scripts/projectEdit.js
+++ scripts/projectEdit.js
@@ -3,10 +3,51 @@ getProjectEdit();
 const url = new URLSearchParams(window.location.search);
 const projID = url.get("uid");
 console.log(projID);
+var docRef = db.collection("projects").doc(projID);
+
+// Console log to see if it works, if it works, we run autoFillValues in the form
+docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+        autoFillValues(doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+
+// autoFillValues();
+
 
 // Autofill the html values with what's in the database! 
+// Query based on the project id.
+function autoFillValues(data){
+    document.getElementById("pName").value = data.name
+    document.getElementById("pDescription").value = data.description
+    document.getElementById("pDueDate").value = data.dueDate
+    document.getElementById("pPriority").value = data.priority
+    document.getElementById("pProgress").value = data.progress
+};
 
 
+// Firestore data converter 
+// https://firebase.google.com/docs/firestore/query-data/get-data
+// var projConverter = {
+//     toFirestore: function(field){
+//         return {
+//             // supposedly these values already exist in the db        
+//             pName: field.pName,
+//             pDescription: field.description,
+//             pDueDate: field.dueDate,
+//             pPriority: field.priority,
+//             pProgress: field.progress,
+//         };
+//     },
+
+// };
 
 
 // Grab all values when 'save' is clicked!
@@ -18,24 +59,23 @@ function getProjectEdit() {
         var pPriority = document.getElementById("pPriority").value
         var pProgress = document.getElementById("pProgress").value
 
+        var data = {
+            "dueDate": pDueDate, 
+            "name":  pName,
+            "description": pDescription,
+            "priority": pPriority,
+            "progress": pProgress}
+
         console.log(pName, pDescription, pDueDate, pPriority, pProgress)     
-        writeProjectEdit(pName, pDescription, pDueDate, pPriority, pProgress);
-        alert("it worked!");
+        writeProjectEdit(data);
         
     })
 }
 
 // Now write this data into our db. 
-function writeProjectEdit(pName, pDescription, pDueDate, pPriority, pProgress){
-    var projects = db.collection("projects")
-    console.log(projects);
-    projects.update({
-        dueDate: pDueDate,
-        name: pName, 
-        description: pDescription,
-        priority: pPriority,
-        progress: pProgress
-    });
+function writeProjectEdit(data){
+    docRef.update(data);
+    console.log("Data successfully updated to DB!")
 }
 
 
